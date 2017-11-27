a4c6ba1a769e5ac657f10dfacc311e2b8e84226b
Rui <rchai@palo-it.com>
2017-11-27 17:08:08 +0800

    changed to get today's date

--- app/routes/Analytics.js
+++ app/routes/Analytics.js
@@ -11,9 +11,18 @@ const routerInstance = new Router()
 // constants
 const prefix = "analytics"
 
+function formatDate(date) {
+
+  const day = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+
+  return year + '/' + month + '/' + day;
+}
+
 routerInstance.get('/', (req, res, next) => {
   const results = db.cypher({
-    query: "MATCH (n:MarginStatement {date:'2017/11/13'})-[r:PART_OF]-(n1:MarginCall) Where not n1.marginAmount is NULL AND n1.side='Client' RETURN n.id, n1.id, n1.marginAmount",
+    query: `MATCH (n:MarginStatement {date:'${formatDate(new Date())}'})-[r:PART_OF]-(n1:MarginCall) Where not n1.marginAmount is NULL AND n1.side='Client' RETURN n.id, n1.id, n1.marginAmount`,
   }, (err, results) => results.length ? res.send({"client": results}) : 'didnt work derp')
 
   // console.log(results)
@@ -21,7 +30,7 @@ routerInstance.get('/', (req, res, next) => {
 
 routerInstance.get('/cpty', (req, res, next) => {
   const results = db.cypher({
-    query: "MATCH (n:MarginStatement {date:'2017/11/13'})-[r:PART_OF]-(n1:MarginCall) Where not n1.marginAmount is NULL AND n1.side='Cpty' RETURN n.id, n1.id, n1.marginAmount",
+    query: `MATCH (n:MarginStatement {date:'${formatDate(new Date())}'})-[r:PART_OF]-(n1:MarginCall) Where not n1.marginAmount is NULL AND n1.side='Cpty' RETURN n.id, n1.id, n1.marginAmount`,
   }, (err, results) => results.length ? res.send({"cpty": results}) : 'didnt work derp')
 })
 
